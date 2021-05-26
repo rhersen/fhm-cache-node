f74ca11b74ad80b7eea346a33e82496178acbc0c
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-05-26 08:21:57 +0200

    add cache without invalidation

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ import http from "http";
 import axios from "axios";
 import xlsx from "xlsx";
 import _ from "lodash";
-import { format, isValid, parse } from "date-fns";
+import { format, isValid, parse, formatDistanceToNow } from "date-fns";
 
 const port = 8080;
+let cache = {};
 
 const server = http.createServer(async (req, res) => {
   console.log(req.method, req.url);
   if (req.url === "/") {
-    const { data, status, statusText } = await axios.get(
-      "https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data",
-      {
-        responseType: "arraybuffer",
-      }
+    console.log(
+      "cached",
+      cache.timestamp ? formatDistanceToNow(cache.timestamp) : "no"
     );
-    console.log(status, statusText, data.length, "bytes");
-    const book = xlsx.read(data);
 
-    res.writeHead(status, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(cases(book)));
+    if (!cache.data) {
+      const { data, status, statusText } = await axios.get(
+        "https://www.arcgis.com/sharing/rest/content/items/b5e7488e117749c19881cce45db13f7e/data",
+        {
+          responseType: "arraybuffer",
+        }
+      );
+      console.log(status, statusText, data.length, "bytes");
+      const book = xlsx.read(data);
+
+      res.writeHead(status, { "Content-Type": "application/json" });
+      cache = { data: cases(book), timestamp: Date.now() };
+    }
+
+    res.end(JSON.stringify(cache.data));
   } else {
     res.writeHead(404);
     res.end();
