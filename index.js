105819b038335e5c6a342d20d619dc6aa8581f78
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-06-04 11:56:48 +0200

    cors

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const server = http.createServer(async (req, res) => {
 
     res.writeHead(200, {
       "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
     });
     res.end(JSON.stringify(cache.data));
   } else {
